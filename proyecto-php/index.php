719c22486b42835a58380d370967e3e09f3c00e4
ibellon <isidro.bellon@gmail.com>
2021-05-05 10:59:30 +0200

    Estilos generales

--- proyecto-php/index.php
+++ proyecto-php/index.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8" />
         <title>Blog de Viedojuegos</title>
+        <link rel="stylesheet" type="text/css" href="./assets/css/styles.css" />
     </head>
     <body>
         <!-- CABECERA -->
